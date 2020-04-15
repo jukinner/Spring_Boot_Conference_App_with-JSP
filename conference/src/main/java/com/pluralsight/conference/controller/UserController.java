15767de456c8914be21b4e210bebb24d42d03f10
jkinner <jkinnera@mastercontrol.com>
2020-04-15 09:16:09 -0600

    added post mapping to User controller

--- conference/src/main/java/com/pluralsight/conference/controller/UserController.java
+++ conference/src/main/java/com/pluralsight/conference/controller/UserController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conference.controller;
 
 import com.pluralsight.conference.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/user")
-    public User getUser(@RequestParam(value = "firstname", defaultValue = "Justin") String firstName,
-                        @RequestParam(value = "lastname", defaultValue = "Kinner") String lastName,
+    public User getUser(@RequestParam(value = "firstName", defaultValue = "Justin") String firstName,
+                        @RequestParam(value = "lastName", defaultValue = "Kinner") String lastName,
                         @RequestParam(value = "age", defaultValue = "37") int age) {
         //obviously this isn't how you would normally do this. Normally you would have a service connected to your DB, which returns something from the repository.
         User user = new User();
@@ -20,6 +21,13 @@ public class UserController {
         user.setAge(age);
 
         return user; //returns the body to the caller.
+    }
+
+    @PostMapping("/user")
+    public User postUser(User user) {
+        System.out.println("User first name:" + user.getFirstName());
+
+        return user;
     }
 
 }
